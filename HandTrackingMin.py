1fe12b3f46d28df5caaf40d9786c3c5c572ab056
Anurag Kulkarni <kanurag0801@gmail.com>
2021-05-16 13:20:12 +0530

    Basic HandTracking Done

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -5,12 +5,25 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while(True):
     #Capture frame by frame
-    ret, frame = cap.read()
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
 
     #Display the resulting frame
-    cv2.imshow('Frame', frame)
+    cv2.imshow('Image', img)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
